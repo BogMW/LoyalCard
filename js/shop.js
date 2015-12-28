7ffe0efc0e4a440e80f2c9f76811d468c3f774b6
BogMW <bogmw.it@gmail.com>
2015-12-29 01:28:42 +0200

    styled shop and added cart total summ

--- js/shop.js
+++ js/shop.js
@@ -26,22 +26,93 @@ var  phones = [
 
 
 function CreatePhone (id){
+    var phone = document.createElement('div');
+    phone.id = 'phone';
+    phone.className = 'phone';
+    phone.style.display = 'inline-block';
+    products.appendChild(phone);
     var images = document.createElement('div');
     var names = document.createElement('div');
     var prices = document.createElement('div');
+    var enter = document.createElement('input');
     images.className = "picture";
     names.className = "name";
     prices.className = "price";
     images.style.background = phones[id].photo + 'no-repeat';
     names.innerHTML = phones[id].name;
     prices.innerHTML = phones[id].price;
-    document.body.appendChild(images);
-    document.body.appendChild(names);
-    document.body.appendChild(prices);
+    enter.type = 'button';
+    enter.value = 'Add to cart';
+    enter.id = id;
+    enter.setAttribute('onclick', "addToCart (this.getAttribute('id'))");
+
+    phone.appendChild(images);
+    phone.appendChild(names);
+    phone.appendChild(prices);
+    phone.appendChild(enter);
 }
 
 function Creator() {
     for (i = 0; i < phones.length; i++) {
         CreatePhone(i);
+    }
+}
+
+function enterToShop(series, number){
+    if (series === 'AA' && number === '0050906598') {
+        document.getElementById('login-popup').style.display = 'none';
+        document.getElementById('cart-series').innerHTML = series;
+        document.getElementById('cart-number').innerHTML = number;
+        return true;
+    } else {
+        alert('User with this card not exist! Check your data.');
+        return false;
+    }
+}
+
+
+var summ = 0;
+var cart = [];
+
+function addToCart (id){
+cart.push(phones[id]);
+    cartSumm(cart);
+    updateCart();
+}
+
+function cartSumm(arr){
+    if(cart.length > 0) {
+        summ = 0;
+        arr.forEach(function (item, i){
+            summ += +item.price;
+        });
+    }
+    return summ;
+}
+
+function showCart() {
+    var cart = document.createElement('div');
+    cart.id = 'cart';
+    cart.className = 'cart';
+    cartContainer.appendChild(cart);
+    var totalPrice = document.createElement('h2');
+    totalPrice.className = 'totalPrice';
+    totalPrice.innerHTML = 'Total price of your order is';
+    cart.appendChild(totalPrice);
+    var orderSumm = document.createElement('div');
+    orderSumm.id = 'orderSumm';
+    orderSumm.class = 'orderSumm';
+    orderSumm.innerHTML = summ + 'UAH';
+    cart.appendChild(orderSumm);
+}
+
+var counter = 0;
+
+function updateCart(){
+    if(counter == 0) {
+        showCart();
+        counter++;
+    } else {
+        orderSumm.innerHTML = summ + 'UAH';
     }
 }
--- shop.php
+++ shop.php
@@ -16,19 +16,28 @@
 require_once ('config.php');
 ?>
 
-<div class="login-popup">
+<div id="login-popup" class="login-popup">
     <div class="popup-container">
         <h1>Enter your series and number</h1>
         <div class="popup-inputs">
-            <input type="text" size="2" maxlength="2">
-            <input type="number" size="2" aria-valuemax="9999999999">
-            <button class="popup-enter">Enter</button>
+            <input id="series" type="text" size="2" maxlength="2" value="AA">
+            <input id="number" type="number" size="2" aria-valuemax="9999999999" value="0050906598">
+            <button class="popup-enter" onclick="enterToShop(document.getElementById('series').value, document.getElementById('number').value)">Enter</button>
         </div>
         <div id="clock" class="clock">
             <span class="hour">00</span>:<span class="min">00</span>:<span class="sec">00</span>
         </div>
     </div>
 </div>
+
+<div id="cartContainer" class="cart-container">
+<div class="cart-id">
+    <div id="cart-series" class="cart-series">XX</div>
+    <div id="cart-number" class="cart-number">9999999999</div>
+</div>
+</div>
+
+
 <div id="products" class="products">
 
 </div>
